8009ccb54f1f7b436218ef3a3aa590a8cf2dfba4
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-05-15 15:53:04 +0500

    changed

---
+++ lesson/lesson 7/index.js
@@ -0,0 +1,21 @@
+let i = 1
+while (true) {
+    if (i % 2) {
+        console.log(i, "toq");
+    }
+    else console.log(i, "juft")
+    i++
+    console.log(i);
+    
+}
+
+let a= 1
+do {
+    if (i % 2) {
+    console.log(i, "toq");
+}
+else console.log(i, "juft")
+i++
+} while (i <= 10)
+
+
