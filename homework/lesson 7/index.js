88c79ef9d5b7d68ea6fa64c3f5e2cba5385ec794
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-05-16 23:23:59 +0500

    bajarildi

---
+++ homework/lesson 7/index.js
@@ -0,0 +1,29 @@
+// for (let i = 0; i < 5; i++) {
+//    console.log(i)
+//   }
+
+//   for (let i = 0; i < 5; ++i) {
+//    console.log(i)
+//   }
+
+
+// let number;
+
+// while (true) {
+//   number = prompt("Iltimos, 100 dan katta son kiriting:");
+
+//   if (number === null || number === "0") {
+//     alert("Kiritish bekor qilindi yoki bo‘sh qator kiritildi.");
+//     break;
+//   }
+
+//   if (+number > 100) {
+//     alert("Rahmat! Siz 100 dan katta son kiritdingiz.");
+//     break;
+//   }
+
+//   alert("Iltimos, faqat 100 dan katta son kiriting.");
+// }
+   
+
+
