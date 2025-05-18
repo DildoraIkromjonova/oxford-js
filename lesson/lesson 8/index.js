7e3e8b681b7af7258f76e5ba54b02b5940938c0e
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-05-18 07:51:44 +0500

    ishladi

--- lesson/lesson 8/index.js
+++ lesson/lesson 8/index.js
@@ -1,5 +1,14 @@
-function funcName() {
-    let name = prompt("Ismingizni kiriting");
-    console.log("salom " + name);
+// function funcName() {
+//     let name = prompt("Ismingizni kiriting");
+//     console.log("salom " + name);
+//   }
+//   funcName();
+
+let num1 =5
+let num2 =10
+
+  function cancAtion(a, b){
+    console.log( a+b)
   }
-  funcName();
+
+cancAtion( num1,num2 )                                                                                       
