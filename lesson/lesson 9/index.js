49bef0eb3e49097a7c41f882f0f3bb22d19f4595
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-05-25 07:13:51 +0500

    changed

---
+++ lesson/lesson 9/index.js
@@ -0,0 +1,19 @@
+ 
+
+ let person ={
+    id:1,
+    name:"Dildora",
+    age:17,
+    status:"O'quvchi"
+ }
+
+ console.log(person.id);
+ console.log(person.name);
+ console.log(person.age);
+ console.log(person.status);
+
+ console.log(person["id"]);
+ console.log(person["name"]);
+ console.log(person["age"]);
+ console.log(person["status"]);
+ 
