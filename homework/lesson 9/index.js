4c6d383edd435fdb9300cc3c5db9ea904c53858d
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-05-26 17:52:42 +0500

    bajarildi

---
+++ homework/lesson 9/index.js
@@ -0,0 +1,15 @@
+// let student = {
+//     firstName: "Eshmat",
+//     lastName: "Toshmatov",
+//     age: 20
+//   };
+  
+//   console.log(student.firstName + " " + student.lastName + " " + student.age);
+
+// let data = {
+//     price: 700,
+//     quantity: 8
+//   };
+  
+//   let total = data.price * data.quantity;
+//   console.log("Umumiy narx: " + total);
