6e048d2dea607287c1e65f8afbb74af34f63c0a9
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-05-31 14:19:52 +0500

    Delete homework directory

--- homework/lesson 6/index.js
+++
@@ -1,8 +0,0 @@
-let yosh = prompt("sizning yoshingiz necida");
-let talaba = prompt("siz talabamisiz");
-
-if (yosh < 18 || talaba == "ha") {
-  console.log("Chipta narxi: 5000 so'm");
-} else {
-  console.log("Chipta narxi: 10000 so'm");
-}
