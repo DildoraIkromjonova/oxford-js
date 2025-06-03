5c7210f8ee3e7f5c9ac5369306a3c71cc2cd29f2
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-06-03 21:26:37 +0500

    bajarildi

---
+++ Homework/homework 3/index.js
@@ -0,0 +1,48 @@
+// console.log(false || true);    //true    
+// console.log(0 || 5);             //5
+// console.log(25|| "Eshmat");     //25
+// console.log(undefined ||null);    //null
+// console.log(0 || undefined);       //undifined
+// ===================================================
+// console.log(true && 0);      // false
+// console.log(5 && 10);            // 10
+// console.log(null && "Eshmat");    // null
+// console.log(1 && undefined);    // undifined
+// // console.log( 15 && false)     // false
+// ====================================================
+// console.log(!false)           //true
+// console.log(!0)               // true
+// console.log(!true)            // false
+// console.log(!1)               // false
+// console.log(!"Eshmat")         // false
+// ====================================================
+//  console.log( false ?? true)   //false
+//  console.log( undefined ?? 1)  // 1
+//  console.log ( 0 ?? 5)        //0
+//  console.log( " Eshmat" ?? 0)  //Eshmat
+//  console.log( null ?? 5)      //5
+// =====================================================
+// =====================================================
+// =====================================================
+// =====================================================
+// let a = (null|| 2 || undefined)
+// console.log(a)
+// ================================
+// let b = ( 1 && null && 2)
+// console.log(b)
+// =================================
+// console.log("" + 1+ 0)
+// console.log("" - 1 +0 )
+// console.log(true+ false)
+// console.log(6 / "3")
+// console.log( "2" * "3")
+// console.log( 4 + 5 + "px")
+// console.log("$" + 4 + 5)
+// console.log("4" - 2)
+// console.log( "4px" - 2)
+// console.log(" -9 " +5)
+// console.log(" -9 " -5)
+// console.log(null + 1)
+// console.log( undefined + 1)
+// console.log("\t \n " -2)
+// =============================
