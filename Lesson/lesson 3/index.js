aa85639bd5d3b672cefab52bfb1f34dfd05d666d
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-06-03 19:17:53 +0500

    changed

---
+++ Lesson/lesson 3/index.js
@@ -0,0 +1,33 @@
+
+// // let a = ("" + 3)
+// // let b = (3 - "")
+// // console.log(a)
+// // console.log(b)
+// // =================
+//  let a = 5 + 3   
+//  console.log(a)
+//  let b = 10 + 7  
+//  console.log(b) 
+// ======================
+//  let a = 9 - 4  
+//  console.log(a)
+//  let b = 20 - 8 
+//  console.log(b)
+// ==========================
+// let a = 6 * 2   
+// console.log(a)
+// let b = 3 * 5 
+// console.log(b)
+// =====================
+// let a = 12 / 6
+// console.log(a)
+// let b = 10 / 5
+// console.log(b)
+// =====================
+// let i = 0
+// console.log(++i)
+// console.log(i++);
+// =======================
+// let j = 0
+// console.log(j--)
+// console.log(--j)
