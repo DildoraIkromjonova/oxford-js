c3ddc3928a0de8254239aa2c363e0a500e730c94
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-06-06 09:53:06 +0500

    changed

---
+++ Lesson/practic lesson/index.js
@@ -0,0 +1,56 @@
+// let number =  Number(prompt( "Iltimos raqam kiriting"));
+// if ( number%2){
+//     console.log("bu son toq =",number )
+// }else{
+//     console.log("bu son Juft =", number)
+// }
+// =============================================
+// let age = prompt("Iltimos yilingizni kiriting")
+// let year = 2025
+// let a = year - age
+// console.log(a)
+// ===============================================
+// let age = Number(prompt("Iltimos Yosh kiriting"))
+
+// if (age >= 2 && age <= 6) {
+//     console.log("Bog'chada o'qiydi")
+// } else if (age >= 7 && age <= 18) {
+//     console.log("Maktab o'qiydi")
+// } else if (age >= 19 && age <= 30) {
+//     console.log("Talaba")
+// } else if (age >= 31 && age <= 60) {
+//     console.log("Ishchi")
+// }else if (age >= 61){
+//     console.log("Nafaqa oluvchi");
+// }
+// else{
+//     console.log("Xato raqam kiritdingiz");
+// // }
+// let a = 3
+// let b = 4
+// let c = 10
+// if( a > b && a > c){
+//     console.log(a , "Katta")
+//     max = a
+// } else if( b > a && b > c){
+//     console.log(b, "Katta")
+//     max = b
+// }else if( c > a && c > b){
+//     console.log( c, "Katta")
+//     max = c
+// }
+// // ===================================
+// if( a < b && a < c){
+//     console.log(a , "Kichik")
+//     min = a
+// } else if( b < a && b < c){
+//     console.log(b, "Kichik")
+//     min = b
+// }else if( c < a && c < b){
+//     console.log( c, "Kichik")
+//     min = c
+// }
+// console.log(max , min )
+// console.log(max - min )
+
+
