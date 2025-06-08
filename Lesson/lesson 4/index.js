ba468373ab0e66c2782e361c123e04652ab29fc1
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-06-08 16:42:53 +0500

    changed

---
+++ Lesson/lesson 4/index.js
@@ -0,0 +1,39 @@
+// let age = 19
+// if( age > 2 && age < 7){
+//     console.log( "Bu bola Bog'chada o'qiydi")
+// }else if ( age >=7 && age <=18){
+//     console.log("Bu bola maktabda o'qiydi")
+// }else if (age >=18 && age <=30){
+//     console.log("Bu bola Talaba")
+// }else{
+//     console.log("siz xato yosh kiritingiz")
+// }
+// ===============================================
+// let a = 7
+// let b = 9
+// let c = 12
+// let max 
+// let min
+// if(a > b && a > c){
+//     console.log(a, "Katta")
+//     max = a
+// }else if (b > a && b > c){
+//     console.log(b, "Katta")
+//     max = b
+// }else if ( c > a && c > b){
+//     console.log( c , "Katta")
+//     max = c
+// }
+// // ===========================
+// if ( a < b && a < c){
+//     console.log( a, "Kichik")
+//     min = a
+// }else if( b < a && b < c){
+//     console.log( b, " Kichik")
+//     min = b
+// }else if ( c < a && c < b ){
+//     console.log( c, "Kichik")
+//     min = c
+// }
+//     console.log( max, min)
+//     console.log( max - min)
