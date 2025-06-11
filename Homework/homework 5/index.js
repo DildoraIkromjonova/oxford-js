8542b605e17aeca06489b3e590e14a365027da79
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-06-11 22:13:26 +0500

    bajarildi

---
+++ Homework/homework 5/index.js
@@ -0,0 +1,20 @@
+// for (let i = 0; i < 5; i++)
+//     console.log(i)
+// for (let i = 0; i < 5; ++i) 
+//     console.log(i)
+// ============================
+// let n = Number(prompt("2 dan katta son kiriting"));
+// for (let i = 2; i <= n; i++) {
+//     let tub = true;
+//     for (let j = 2; j < i; j++) {
+//         if (i % j === 0) {
+//             tub = false;
+//             break;
+//         }
+//     }
+//     if (tub) {
+//         console.log(i);
+//     }
+// }
+// ===============================================
+
