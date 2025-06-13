632e4927f17453681d02425ba2976b3eb09e593c
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-06-13 16:21:20 +0500

    changed

---
+++ Homework/homework 6/index.js
@@ -0,0 +1,20 @@
+// let i = 0;
+//  while( i < 5 ){
+// console.log(i)
+// i++
+// }
+// let i = 0
+// do{
+//     console.log(i)
+//     ++i
+// } while(i < 5)
+// =================================================
+// let number;
+// do {
+//   number = prompt("Iltimos, 100 dan katta son kiriting:", "");
+// } while (number !== null && number !== "" && +number <= 100);
+// ================================================================
+// let number = prompt("Iltimos, 100 dan katta son kiriting:", "");
+// while (number !== null && number !== "" && +number <= 100) {
+//   number = prompt("Iltimos, 100 dan katta son kiriting:", "");
+// }
