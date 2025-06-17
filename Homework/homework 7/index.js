d22a8285757895bfc980839f2d09e3aa98dc0f51
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-06-17 14:26:01 +0500

    bajarildi

--- Homework/homework 7/index.js
+++ Homework/homework 7/index.js
@@ -1,9 +1,12 @@
 // let a = 2;
 // let b = 8;
-// let c = a * b;
-// console.log(c);
-// ============================
-// let a = 5;
-// let b = 9;
-// let c = (a + b) / 2
-// console.log(c);
+// let e = 5;
+// let c = a + b + e;
+// console.log(c/2);
+// =============================
+// let a = 12
+// let b = 4
+// let c = 5
+// let kopaytma = a * b * c;
+// let natija = kopaytma ** (1 / 3);
+// console.log(natija);
