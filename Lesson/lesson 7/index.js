ca588f8f8dc6a5dadf1636d1089baa81df90a754
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-06-18 19:28:18 +0500

    changed

---
+++ Lesson/lesson 7/index.js
@@ -0,0 +1,32 @@
+// let a = 3
+// let b = 9
+// let c = 27
+// let d = 81
+// function getData( param1,param2,param3,param4){
+//     let arifmetik = (3 + 9 + 27 + 81) / 4;
+//     console.log(arifmetik)
+// }
+// getData( a, b, c, d,)
+// ====================================
+// let a = 3
+// let b = 9
+// let c = 27
+// let d = 81
+// function getData( param1,param2,param3,param4){
+//     let geometrik = (3 * 9 * 27  * 81) ** (1/4)
+//     console.log(geometrik)
+// }
+// getData( a, b, c, d,)
+// =======================================
+// object ikki turga bolinadi 
+// obj{}
+// obj = new Object (construkshin)
+// ====================================
+// let obj = {
+//     id:12,
+//     name:"Eshmat",
+//     status:"Talaba",
+//     age:25
+// }
+// console.log(obj.age)
+// console.log(obj["name"])
