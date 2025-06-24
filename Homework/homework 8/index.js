d560e9b5b85d41163ee085bacae8f6f1c4f1f119
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-06-24 10:44:44 +0500

    changedd

---
+++ Homework/homework 8/index.js
@@ -0,0 +1,27 @@
+// function min(a, b) {
+//     if (a < b) {
+//       return a;
+//     } else {
+//       return b;
+//     }
+//   }
+//   console.log(min(2, 5));   
+//   console.log(min(3, -1));  
+//   console.log(min(1, 1));   
+// ===================================
+// 1)
+// function pow(a, b) {
+//     return a ** b; 
+//   }
+//   console.log(pow(3, 2));
+// 2)
+// function pow(a, b) {
+//     return a ** b;
+//   }
+//   console.log(pow(3, 3)); 
+// 3)
+// function pow(a, b) {
+//     return a ** b;
+//   }
+//   console.log("pow(1, 100) == " + pow(1, 100)); 
+// ========================================================
---
+++ Homework/homework 9/index.js
@@ -0,0 +1,15 @@
+// let student = {
+//     firstName: "Eshmat",
+//     lastName: "Toshmatov",
+//     age: 20,
+//     course: "Frontend"
+//   };
+// console.log(student.firstName + " " + student.lastName + " " + student.age);
+// ================================================================================
+// let data = {
+//     name: "Phone",
+//     model: "iPhone",
+//     price: 700,
+//     quantity: 8
+//   };
+//   console.log( data.price * data.quantity);
