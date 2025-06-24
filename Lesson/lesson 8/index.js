65a2b0db9167832778080b6e9006ac9fe3a99de9
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-06-24 10:12:47 +0500

    changed

---
+++ Lesson/lesson 8/index.js
@@ -0,0 +1,30 @@
+// let obj = {
+//     name:"Eshmat",
+//     id:1,
+// }
+// console.log(obj.name)
+// console.log(obj["id"])
+// // Static and Dinamic
+
+// // Static
+// console.log(obj.name);
+
+// // Dinamic
+// console.log(obj["id"]);
+
+// let ism = "yoshi";
+
+// console.log(obj["ism"]);
+
+// Object assign 2 va undan ortiq objectlarni bir birga qoshib beradi.
+// yuzaki nusxa olib beradi
+
+
+// let obj1 = {
+//     name: "Eshmat",
+// }
+// let obj2 = {
+//     id:12
+// }
+
+// let newObj = Object.assign(obj1,obj2)
---
+++ Lesson/lesson 9/index.js
@@ -0,0 +1,40 @@
+// let obj1 = {
+//   name: "Ali",
+// };
+// let obj2 = {
+//   name: "Ali",
+// };
+// console.log(obj1 === obj2);
+// console.log(obj1 == obj2);
+
+// let a = 10
+// let b = 10
+
+// console.log(a === b)
+// console.log(a == b)
+
+// let obj = {
+//     name: "Toshmat"
+// }
+
+// // let cloneObj = Object.assign({}, obj);
+// let cloneObj = structuredClone(obj);
+
+// cloneObj.name = "Eshmat";
+
+// console.log(obj);
+// console.log(cloneObj);
+
+
+// let obj = {
+//     name: "Eshmat",
+//     id: 12,
+//     status: "talaba",
+//     age: 20,
+// }
+// // console.log(obj.name);
+
+
+// for(let key in obj){
+//     console.log(key, obj[key])
+// }
