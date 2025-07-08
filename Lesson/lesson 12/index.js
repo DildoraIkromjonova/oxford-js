b7e50b2ae31d0e81f2e7c2659eac3d1808ce60d0
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-07-08 11:50:17 +0500

    changed

---
+++ Lesson/lesson 12/index.js
@@ -0,0 +1,38 @@
+// let yigindi = 0
+// let juft = 0
+// let toq = 0
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// for (let value of arr) {
+//     yigindi = yigindi + value
+
+//     if (value % 2) {
+//         juft = juft + value
+//     } else {
+//       toq = toq + value
+//     }
+// }
+// console.log(yigindi)
+// console.log(juft)
+// console.log(toq)
+// =========================
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+// let tubSonlar = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//   let son = arr[i];
+//   let tub = true;
+//   if (son < 2) tub = false;
+//   for (let j = 2; j < son; j++) {
+//     if (son % j == 0) {
+//       tub = false;
+//       break;
+//     }
+//   }
+
+//   if (tub) {
+//     tubSonlar.push(son);
+//   }
+// }
+
+// console.log("Tub sonlar:", tubSonlar);
+// =======================================
