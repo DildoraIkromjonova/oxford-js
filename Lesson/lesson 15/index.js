1587482aff4469adf476e8cae2110e63bd741ec0
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-07-16 23:52:17 +0500

    changed

---
+++ Lesson/lesson 15/index.js
@@ -0,0 +1,40 @@
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let sum = 0;
+// arr.forEach((value) => {
+//   sum += value;
+// });
+// console.log(sum); 
+// // ===============================
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let newArr= arr.map((value,index,arr)=>{
+//     return value
+// })
+// console.log(newArr)
+// // ========================================
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let newArr = arr.filter(num => num % 2 === 0);
+// console.log( newArr); 
+// let arr = [1, 2, 3];
+// arr.forEach(function(value, index) {
+//   console.log(index, value, arr);
+// });
+// ===========================================
+// let arr = [1, 2, 3];
+// let result = arr.map(x => x * 2);
+// console.log(result);
+// =============================================
+// let arr = [1, 2, 3, 4, 5];
+// let result = arr.filter(x => x % 2 === 0);
+// console.log(result)
+// ============================================
+// let arr = [5, 12, 8, 130, 44];
+// let found = arr.find(x => x > 10);
+// console.log(found);
+// ============================================
+// let arr = [5, 12, 8, 130, 44];
+// let index = arr.findIndex(x => x > 100);
+// console.log(index);
+// =============================================
+// let arr = [5, 12, 8, 130, 44];
+// let index = arr.findLastIndex(x => x > 10);
+// console.log(index);
