d70f5dfe4d8e0335adc19e0962b829974fb8e771
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-07-19 10:32:28 +0500

    bajarildi

---
+++ Homework/homework 16/index.js
@@ -0,0 +1,16 @@
+
+//   let users = [
+//     { id: 2, name: "Ali", age: 25 },
+//     { id: 1, name: "Sardor", age: 27 },
+//     { id: 3, name: "Bek", age: 23 }
+//   ];
+// //   ==========================
+//   users.sort((a, b) => a.age - b.age);
+//   console.log("Age", users);
+// //   ==========================
+//   users.sort((a, b) => a.name.localeCompare(b.name));
+//   console.log("Name", users);
+// //   ========================
+//   users.sort((a, b) => a.id - b.id);
+//   console.log("ID", users);
+// ========================================================================
