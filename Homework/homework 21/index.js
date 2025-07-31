b8443f1a55aa0e081cf3dad04c6771d540ca3126
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-07-31 14:05:35 +0500

    changed

---
+++ Homework/homework 21/index.js
@@ -0,0 +1,41 @@
+let todos = [];
+
+document.getElementById("add-btn").addEventListener("click", addTodo);
+document.getElementById("clear-btn").addEventListener("click", clearAll);
+
+function addTodo() {
+  const input = document.getElementById("todo-input");
+  const value = input.value.trim();
+  if (value !== "") {
+    todos.push(value);
+    input.value = "";
+    renderTodos();
+  }
+}
+
+function deleteTodo(index) {
+  todos.splice(index, 1);
+  renderTodos();
+}
+
+function clearAll() {
+  todos = [];
+  renderTodos();
+}
+
+function renderTodos() {
+  const list = document.getElementById("todo-list");
+  list.innerHTML = "";
+
+  todos.forEach((todo, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      ${todo}
+      <button class="delete-btn" onclick="deleteTodo(${index})">🗑️</button>
+    `;
+    list.appendChild(li);
+  });
+
+  document.getElementById("task-count").innerText = 
+    `You have ${todos.length} pending ${todos.length === 1 ? "task" : "tasks"}`;
+}
---
+++ Lesson/lesson 21/index.js
@@ -0,0 +1,8 @@
+let myInput = document.querySelector(".myInput")
+let myBtn = document.querySelector(".myBtn")
+
+console.log(myInput)
+
+myBtn.addEventListener ("click", ()=> {
+  console.log("Button ishladi")
+})
