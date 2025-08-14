1a8748253782dc65e035658fb71bceed97034b0d
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-08-14 11:01:53 +0500

    bajarildi

---
+++ test/index.js
@@ -0,0 +1,63 @@
+
+// const sum = (num) => {
+//   if (num > 0) {
+//   return"musbat";
+//   } else if (num < 0) {
+//     return"manfiy";
+//   } 
+// }
+
+// console.log(sum(-5))
+
+
+// let summ = (num)=> num % 2 ? "toq" : "juft"
+// console.log(summ(3))
+//  const summ = (a, b, c) => {
+//  if(a > b && a > c){
+//   return a;
+//  }else if (b > a && b > c){
+//   return b;
+//  } else {
+//   return c;
+//  }
+//  };
+//  console.log(summ(2, 5, 9))
+// ========================================
+// 5
+// let sum = () => {
+//   let summa = 120
+//   if (summa > 100) {
+//       console.log( summa * 0.1);
+//   } else if (summa >= 50) {
+//       console.log( summa * 0.05);
+//   } else {
+//       console.log();
+//   }
+// }
+// sum()
+// ======================================================
+
+// 6
+// let Tugilganyilnianqilash = () => {
+//   let tugilganYil = 2007
+//   let yosh = new Date().getFullYear() - tugilganYil;
+//   if (yosh < 18) {
+//       console.log("Voyaga yetmagan");
+//   } else {
+//       console.log(yosh);
+//   }
+// }
+// Tugilganyilnianqilash()
+// =================================================================
+// // 8
+// const haroratAniqlash = () => {
+//   let temp = 25;
+//   if (temp < 0) {
+//       console.log("Muzlik");
+//   } else if (temp <= 30) {
+//       console.log("Yoqimli harorat");
+//   } else {
+//       console.log("Juda issiq");
+//   }
+// }
+// haroratAniqlash()
